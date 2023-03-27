68a2355290624e4e6bc949321e24ede0f3453d48
elrilacho <camilobr17@gmail.com>
2023-03-27 09:39:44 -0500

    Ajustes listos CF ok

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -132,7 +132,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/_Rcve-n36U0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .row.mt-5
       h3 Aprovisionamiento y solicitud
